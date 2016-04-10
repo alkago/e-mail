ba18fb286e8022d08ae8b60f05c4d4bb7f14ad1e
alkago <jay724@nate.com>
2016-04-10 17:12:50 +0000

    1

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,45 @@
+class HomeController < ApplicationController
+    
+    
+    def index
+        
+    end
+    def send_1
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+    
+    end
+    def send_2
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+    
+    end
+    def send_3
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+        
+    end
+    def send_4
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+
+    end
+    def send_5
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+        
+    end
+    def send_6
+        @board_target = params[:target]
+        @board_title = params[:title]
+        @board_content = params[:content]
+        
+    end
+
+
+end
